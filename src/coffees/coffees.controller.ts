92d22031e0fc0d3fdbc3ba86b30cd2e0d7342e63
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-07-10 15:06:06 -0300

    Handling Request Body / Payload

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -10,5 +10,13 @@ export class CoffeesController {
     @Get(':id') // dynamic route
     findOne(@Param('id') id: string) {
         return `This action returns #${id} coffee`;
+    }
+
+    @Post()
+    create(@Body() body) {
+        return {
+            message: 'This action adds a new coffee',
+            body,
+        };
     }
 }
