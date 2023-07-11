f22775e4624d496e9a9b78e95d346daae8e64223
Gabriel Messias <gabrielmessiasdarosa@gmail.co>
2023-07-11 15:42:15 -0300

    Auto-transform Payloads to DTO instances

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // remove all the properties that are not in the DTO
     forbidNonWhitelisted: true, // throw an error if a property that is not in the DTO is sent
+    transform: true, // transform the data to the type of the DTO
   }));
   await app.listen(8080);
 }
