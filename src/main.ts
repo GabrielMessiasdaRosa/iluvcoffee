2b142aa9b654e8e9b365f3930bb4fb8bcf97e624
Gabriel Messias <gabrielmessiasdarosa@gmail.co>
2023-07-11 15:36:53 -0300

    Handling Malicious Request Data

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // remove all the properties that are not in the DTO
+    forbidNonWhitelisted: true, // throw an error if a property that is not in the DTO is sent
+  }));
   await app.listen(8080);
 }
 bootstrap();
