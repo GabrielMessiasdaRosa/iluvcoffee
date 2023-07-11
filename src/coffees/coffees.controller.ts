8d768ebde006d542f3aa7f67de27b0da4798ef17
Gabriel Messias <gabrielmessiasdarosa@gmail.co>
2023-07-10 23:43:29 -0300

    Handling Update and Delete Requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,22 +1,45 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get()
-    findAll() {
-        return 'This action returns all coffees';
-    }
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
 
-    @Get(':id') // dynamic route
-    findOne(@Param('id') id: string) {
-        return `This action returns #${id} coffee`;
-    }
+  @Get(':id') // dynamic route
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
 
-    @Post()
-    create(@Body() body) {
-        return {
-            message: 'This action adds a new coffee',
-            body,
-        };
-    }
+  @Post()
+  create(@Body() body) {
+    return {
+      message: 'This action adds a new coffee',
+      body,
+    };
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return {
+      message: `This action updates #${id} coffee`,
+      body,
+    };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return {
+      message: `This action removes #${id} coffee`,
+    };
+  }
 }
