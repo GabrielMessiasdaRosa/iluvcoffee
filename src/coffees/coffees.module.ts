7b3041db8a721ac578a87fa2ffaa1792821b1508
Gabriel Messias <gabrielmessiasdarosa@gmail.co>
2023-07-13 18:22:47 -0300

    Value based Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,10 +6,18 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffees.entity';
 import { Flavor } from './entities/flavor.entity';
 
+class MockCoffeesService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: CoffeesService,
+      useValue: new MockCoffeesService(), // <-- mock implementation
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
