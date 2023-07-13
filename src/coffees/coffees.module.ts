c6b789a01b932df88d29aa6289f38eb3e91e7dfa
Gabriel Messias <gabrielmessiasdarosa@gmail.co>
2023-07-13 18:27:26 -0300

    Non-class-based Provider Tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffees.entity';
 import { Flavor } from './entities/flavor.entity';
-
-class MockCoffeesService {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
@@ -14,8 +13,8 @@ class MockCoffeesService {}
   providers: [
     CoffeesService,
     {
-      provide: CoffeesService,
-      useValue: new MockCoffeesService(), // <-- mock implementation
+      provide: COFFEE_BRANDS,
+      useValue: ['buddy brew', 'nescafe'],
     },
   ],
   exports: [CoffeesService],
