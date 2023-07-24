3c1de6cc9bc2c52bc53d95a6a599089702c6fd6d
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-07-24 15:43:15 -0300

    fix: missing forroot prop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,16 @@
-import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeesModule } from './coffees/coffees.module';
+import appConfig from './config/app.config';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-      }),
+      load: [appConfig],
     }),
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
