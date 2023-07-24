4a2ffdc72b200e8f480996bf9ed5b11e92ae42d7
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-07-24 14:51:14 -0300

    fix: missing import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeesModule } from './coffees/coffees.module';
 import { DatabaseModule } from './database/database.module';
-
 
 @Module({
   imports: [
