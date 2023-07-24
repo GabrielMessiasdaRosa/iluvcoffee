877efc1a0eab4444b8719b69f6880c6db3d08e41
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-07-24 15:50:30 -0300

    Asynchronously Configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,26 +5,27 @@ import { AppController } from './app.controller';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeesModule } from './coffees/coffees.module';
 import appConfig from './config/app.config';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres', // type of our database
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true, // models will be loaded automatically
+        synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig],
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres', // type of our database
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true, // models will be loaded automatically
-      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
-    }),
+
     CoffeesModule,
     CoffeeRatingModule,
-    DatabaseModule,
   ],
   controllers: [AppController],
 })
