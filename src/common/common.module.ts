0ba02e049d54a493d1ea5e150fe7fc3e1489bf89
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-12-20 12:13:07 -0300

    feat: :sparkles: Add ApiKeyGuard and LoggingMiddleware to CommonModule

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 
-// to use the ApiKeyGuard guard, we need to add it to the providers array of the CommonModule and add the APP_GUARD provider 
+// to use the ApiKeyGuard guard, we need to add it to the providers array of the CommonModule and add the APP_GUARD provider
 // to the providers array of the CommonModule and set the useClass property of the APP_GUARD provider to the ApiKeyGuard guard
 
 @Module({
   imports: [ConfigModule],
   providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
 })
-export class CommonModule {}
+// to use the LoggingMiddleware middleware, we need to add it to the providers array of the CommonModule and add the APP_GUARD provider
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggingMiddleware)
+      .forRoutes({ path: 'coffees', method: RequestMethod.GET }); // apply the LoggingMiddleware middleware to the coffees route
+  }
+}
